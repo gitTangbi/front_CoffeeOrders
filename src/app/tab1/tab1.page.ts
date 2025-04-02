71dfb67104bb2b130966e811fe96e1ed37a843e5
Bill Tang <tangbi@sheridancollege.ca>
2025-04-01 21:33:26 -0400

    Mostly finished tab1, added some other classes that'll be useful.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,43 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  temp: any;
+  milk: any;
+  sweetness: any;
+  brew: any;
+  size: any;
 
+  responseMsg: any = [];
+
+  constructor(private service: OrderService) {
+    //Default values
+    this.temp = "Hot";
+    this.milk = "2";
+    this.sweetness = "100";
+    this.brew = "Regular";
+    this.size = "Medium";
+  }
+
+
+  insert(){
+    let data = {
+      temp: this.temp, 
+      milk: this.milk, 
+      sweetness: this.sweetness, 
+      brew: this.brew, 
+      size: this.size
+    }
+    
+    // this.service.insert(data)({
+    //   next:(data:any) =>{
+    //     console.log(data);
+    //     this.responseMsg = data.message;
+    //   },
+    //   error:(e) =>{
+    //     console.error(e);
+    //     this.responseMsg = e.message;
+    //   },
+    //   complete: () => console.info('Successfully added!');
+    // })
+  }
 }
