adb91ab7e264ca190827d7188d78f0455145cd4f
DamanpreetChahal <preetdamankaur196@gmail.com>
2025-04-05 03:21:21 -0400

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OrderService } from '../order.service';
-
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -15,16 +16,24 @@ export class Tab1Page {
   brew: any;
   size: any;
 
-  responseMsg: any = [];
+  outMsg:any;
 
-  constructor(private service: OrderService) {
+  responseMsg: any = [];
+  orders:any=[];
+  id:number=1;
+  result?:String;
+  constructor(private service: OrderService, private ar:ActivatedRoute, private r:Router) {
     //Default values
     this.temp = "Hot";
     this.milk = "2";
     this.sweetness = "100";
     this.brew = "Regular";
     this.size = "Medium";
+    this.id=1;
+   
   }
+
+  
 
 
   insert(){
@@ -33,9 +42,32 @@ export class Tab1Page {
       milk: this.milk, 
       sweetness: this.sweetness, 
       brew: this.brew, 
-      size: this.size
+      size: this.size,
+      id:this.id
     }
+    this.service.insert(data).subscribe({
+      next: (data:any)=>{
+        console.log(data);
+        this.orders=[];
+        this.outMsg="Order Added!!"
+        
+      },
+      error:(e)=>{
+        console.error(e);
+        this.outMsg="Error adding order"
+
+      },
+      complete:()=>console.info('Complete')
+    });
+
+
+    //the above will add the order and the below will show the order added in the database
+  
+   this.retrieveOne();
+
     
+
+  }
     // this.service.insert(data)({
     //   next:(data:any) =>{
     //     console.log(data);
@@ -47,5 +79,29 @@ export class Tab1Page {
     //   },
     //   complete: () => console.info('Successfully added!');
     // })
+
+   retrieveOne(){
+  
+      const params={id:this.id};  
+      console.log("Params being sent:", params);
+      this.service.retrieveOne(params).subscribe({
+        next:(data:any)=>{
+          console.log( "data sent ",  data);
+          this.orders=data;
+        
+        },
+        error:(e)=>{
+          console.error(e);
+  
+        },
+        complete:()=>console.info('Complete')
+      });
+      
+    }
+
+    nextOrder(){
+      this.id++;
+      this.outMsg="";
+    }
   }
-}
+
